a0f3622646803f4ea6a29587eb28ad22d2d31c6e
eugenennamdi <eugenennamdi7@gmail.com>
2026-07-14 08:21:08 +0100

    clean up scratch files

--- test-init-invalid.js
+++
@@ -1,7 +0,0 @@
-const { OKXFacilitatorClient } = require('@okxweb3/app-x402-core');
-const client = new OKXFacilitatorClient({
-  apiKey: "invalid",
-  secretKey: "invalid",
-  passphrase: "invalid",
-});
-client.getSupported().then(console.log).catch(console.error);
